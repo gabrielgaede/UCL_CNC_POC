e2d9fe9e2bbce5232bddfba6aeba320a4120bace
Gabriel Gaede <gabriel.gaede@troika.ind.br>
2020-05-30 12:35:54 -0300

    Envio backend

--- HttpTriggerCnC/index.js
+++ HttpTriggerCnC/index.js
@@ -1,16 +1,47 @@
+'use strict';
+
+const { Client } = require('azure-iothub');
+
+const executeRemoteCommand = (deviceId, connectionString) => {
+  const client = Client.fromConnectionString(connectionString);
+
+  const methodParams = {
+    methodName: 'Ping',
+    payload: {},
+    responseTimeoutInSeconds: 30
+  };
+
+  return new Promise((resolve, reject) => {
+    client.invokeDeviceMethod(deviceId, methodParams, function (err, result) {
+      err ? reject(err) : resolve(result);
+    });
+  });
+};
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.command || (req.body && req.body.command)) {
+    try {
+        const result = await executeRemoteCommand( 'uclBotDevice', 'HostName=ucl-iot-hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=JoIaQf1v8kZjJ4YmGYLSYJ7ZLIZhndBsrsY4/5U5KFw=');
+        context.log(`Received response: ${result}`);
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Your Command: " + (req.query.command || req.body.command)
+            headers: {
+            "Content-Type": 'application/json'
+            },
+            body: result
         };
-    }
-    else {
+        } catch (err) {
+        context.log(err);
         context.res = {
-            status: 400,
-            body: "Please pass a command on the query string or in the request body"
+            status: 500,
+            headers: {
+            "Content-Type": 'application/json'
+            },
+            body: {
+            message: "Failed to execute the remote command",
+            details: err.message
+            }
         };
-    }
+        }
+
 };
---
+++ client/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const connectionString = 'HostName=ucl-iot-hub.azure-devices.net;DeviceId=uclBotDevice;SharedAccessKey=3VokBSqCmOfCP2gZxRUbv5sC+1dhQsN8drmiDH81ddo=';
+
+var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
+var Message = require('azure-iot-device').Message; 
+var client = clientFromConnectionString(connectionString);
+
+var connectCallback = function (err) {
+    if (err) {
+      console.error('Could not connect: ' + err);
+    } else {
+      console.log('Client connected');
+      var message = new Message('some data from my device');
+      client.sendEvent(message, function (err) {
+        if (err) console.log(err.toString());
+      });
+   
+      client.on('message', function (msg) { 
+        console.log(msg); 
+        client.complete(msg, function () {
+          console.log('completed');
+        });
+      }); 
+    }
+  };
+
+  client.open(connectCallback);
+
