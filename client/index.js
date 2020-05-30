07b1226818d8f40eff8f30778b032af67a416e20
Gabriel Gaede <gabriel.gaede@troika.ind.br>
2020-05-30 18:32:35 -0300

    Finalizacao do Client

--- client/index.js
+++ client/index.js
@@ -7,10 +7,22 @@ var Message = require('azure-iot-device').Message;
 var client = clientFromConnectionString(connectionString);
 
 var connectCallback = function (err) {
+
+    client.onDeviceMethod('Ping', (request, response) => {
+      response.send(200, 'Ping-ponging', err => {
+        if(err) {
+          console.error('An error occurred when sending a method response:\n' + err.toString());
+        } else {
+          console.log('Response to method \'' + request.methodName + '\' sent successfully.' );
+        }
+      });
+
+    });
+
     if (err) {
       console.error('Could not connect: ' + err);
     } else {
-      console.log('Client connected to IoT Hub');
+      console.log('Client connected');
       var message = new Message('some data from my device');
       client.sendEvent(message, function (err) {
         if (err) console.log(err.toString());
